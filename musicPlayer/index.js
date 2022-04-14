3b9054a46ad0b8b83180df8f0b590d202a89c0a4
Abhinit Kumar <50126584+abhinit1999@users.noreply.github.com>
2022-04-14 13:30:00 +0530

    Add files via upload

---
+++ musicPlayer/index.js
@@ -0,0 +1,13 @@
+
+// let id1 = document.getElementById('0');
+let marqId = document.getElementById('marq');
+let song1 = document.getElementById('s1');
+let song1txt = document.getElementById("s1txt");
+song1.addEventListener('click',()=>
+{
+    marqId.innerHTML=song1txt;
+}
+
+)
+
+
